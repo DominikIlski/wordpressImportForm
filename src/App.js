641bce781ad94723fa5cf99410aa8e97e87b815f
DominikIlski <dominik.franciszek.ilski@gmail.com>
2020-07-29 13:36:40 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, /*useEffect*/ } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+const App = () => {
+  const [flotiqApiKey, setFlotiqApiKey] = useState('');
+  const [wordpressUrl, SetWordpressUrl] = useState('');
+  const submit = () => {
+    console.log('Button pressed')
+    console.log("Flotiq API Key: " + flotiqApiKey)
+    console.log("Wordpress URL:  " + wordpressUrl)
+
+    const requestOptions = {
+      method: 'POST',
+      body: {
+        flotiqApiKey:  flotiqApiKey ,
+        wordpressUrl:  wordpressUrl 
+      }
+    }
+
+    console.log(requestOptions)
+    fetch('http://127.0.0.1:3000/hello', requestOptions)
+    .then(console.log('-------------------response-----------------'))    
+    .then(res => console.log(res.json()))
+  }
+  /*
+  {
+      "flotiqApiKey": "26f841d43814d82c01c885b899fb475b",
+      "wordpressUrl": "https://wordpress-mysql.dev.cdwv.pl/"
+  }
+  */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Flotiq WordPress importer!</h1>
+      <form onSubmit={submit} >
+        <label>
+          <h3>Flotiq Read and Write API Key:</h3>
+          <input type="text" value={flotiqApiKey}
+            onChange={(event) => setFlotiqApiKey(event.target.value)} />
+
+        </label>
+        <br />
+        <label>
+          <h3>WordPress full URL:</h3>
+          <input type="text" value={wordpressUrl}
+            onChange={(event) => SetWordpressUrl(event.target.value)} />
+
+        </label>
+        <br />
+        <input type="button" value="Import to Flotiq" onClick={submit} />
+
+      </form>
     </div>
   );
 }
